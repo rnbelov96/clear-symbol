1140238f99b007a3b46b065998fbdd66dac44b12
Roman Belov <rnbelov96@gmail.com>
2021-07-09 10:58:39 +0300

    Добавляет critical css

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlCriticalWebpackPlugin = require('html-critical-webpack-plugin');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -47,5 +49,18 @@ module.exports = merge(common, {
     new MiniCssExtractPlugin({
       filename: 'css/[name].css',
     }),
+    new HtmlCriticalWebpackPlugin({
+      base: path.resolve(__dirname, 'dist'),
+      src: 'index.html',
+      dest: 'index.html',
+      inline: true,
+      minify: true,
+      extract: true,
+      width: 375,
+      height: 565,
+      penthouse: {
+        blockJSRequests: false,
+      }
+    })
   ],
 });
